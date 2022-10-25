e25755b64e62212d74c4cc25f5e1f5b3c5624742
RailsMoocUser <rails-mooc@example.com>
2022-10-25 17:15:44 +0000

    add viikko3/tehtävä8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,9 +1,9 @@
 class Brewery < ApplicationRecord
+  include ActiveModel::Validations
   include RatingAverage
 
   validates :name, presence: true
-  validates :year, numericality: {  in: 1040..2022,
-                                    only_integer: true }
+  validate :prohibited_year_range
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
@@ -17,5 +17,19 @@ class Brewery < ApplicationRecord
   def restart
     self.year = 2022
     puts "changed year to #{year}"
+  end
+
+  def prohibited_year_range
+    # 1. check that year contains an integer value
+    # 2. check that year >= 1040
+    # 3. check that year is <= current year
+
+    # Inspiration from https://stackoverflow.com/a/9075606
+    # Pattern dddd, where the first digit is a non-zero value
+    errors.add(:year, "must be an integer value") unless year =~ /^[1-9][0-9]{3}$/
+
+    errors.add(:year, "must be greater than 1040 ") if year.to_i < 1040
+
+    errors.add(:year, "must be less than #{Time.now.year}") if year.to_i > Time.now.year
   end
 end
