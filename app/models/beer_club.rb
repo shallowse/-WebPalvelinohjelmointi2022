66c7f0b381b3a3fb240753ab76d41cb1fda347ac
RailsMoocUser <rails-mooc@example.com>
2022-10-27 16:30:45 +0300

    add fix from model answers

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -1,6 +1,6 @@
 class BeerClub < ApplicationRecord
-  has_many :memberships
-  has_many :users, through: :memberships
+  has_many :memberships, dependent: :destroy
+  has_many :members, through: :memberships, source: :user
 
   def to_s
     name.to_s
