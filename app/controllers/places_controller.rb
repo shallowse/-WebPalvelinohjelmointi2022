7d55627ec94285a035da6ec9139ba5fd7525f47f
RailsMoocUser <rails-mooc@example.com>
2022-10-29 17:52:30 +0300

    add vk4/tehtävät 5-6

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,11 +2,18 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def show
+    city = session[:last_searched_city]
+    p = Rails.cache.read(city)
+    @place_details = p.select { |place| place.id == params[:id] }.first
+  end
+
   def search
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
+      session[:last_searched_city] = params[:city]
       render :index, status: 418
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,6 @@ Rails.application.routes.draw do
 
   # get "kaikki_bisset", to: "beers#index"
 
-  get 'places', to: 'places#index'
+  resources :places, only: %i[index show]
   post 'places', to: 'places#search'
 end
