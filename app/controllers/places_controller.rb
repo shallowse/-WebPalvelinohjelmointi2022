3e245b1fd520c4e95346637a1bde1fd37082dcf8
RailsMoocUser <rails-mooc@example.com>
2022-10-29 18:04:21 +0300

    fix, do not permit empty parameter

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,6 +9,7 @@ class PlacesController < ApplicationController
   end
 
   def search
+    params.require(:city)
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
