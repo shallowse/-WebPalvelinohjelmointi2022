8e49f02a0be829dca40939d4fa0ea0a6338e6f77
RailsMoocUser <rails-mooc@example.com>
2022-10-30 16:35:42 +0200

    add mods

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -8,6 +8,9 @@ class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  scope :active, -> { where active: true }
+  scope :retired, -> { where active: [nil, false] }
+
   def print_report
     puts name
     puts "establised at year #{year}"
