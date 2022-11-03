88f55c2e6e857533256acf855e4c214d8600f6d7
RailsMoocUser <rails-mooc@example.com>
2022-11-03 13:54:42 +0200

    add index to table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_30_191639) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_115220) do
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
     t.integer "founded"
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_191639) do
     t.string "password_digest"
     t.boolean "admin"
     t.boolean "active"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
